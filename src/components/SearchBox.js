2a5e4ae33256df46ecc156dc2e44d2669c5ceaf3
Antonio Mateo Rios <mathewrios61@gmail.com>
2019-04-07 11:03:08 +0200

    cambios search box

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -8,8 +8,8 @@ class SearchBox extends Component {
       <Form>
         <Form.Group controlId="formBasicEmail" className="m-0 position-relative search-box">
           <Form.Label className="sr-only">Escribe algo...</Form.Label>
-          <Form.Control type="text" className="rounded-pill border-0 pl-5 gray font-bold" placeholder="Escribe algo..." />
-          <FontAwesomeIcon icon="search" className="position-absolute gray icon-search" size="lg" />
+          <Form.Control type="text" className="rounded-pill border-0 gray font-bold" placeholder="Escribe algo..." />
+          <FontAwesomeIcon icon="search" className="position-absolute gray icon-search" size="sm" />
         </Form.Group>
       </Form>
     );
